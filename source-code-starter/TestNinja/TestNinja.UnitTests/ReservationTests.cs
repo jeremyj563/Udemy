329af66bfd09e8b630b3b4ce662df4c9d1585345
Jeremy Johnson <dummy.bin@hotmail.com>
2020-06-13 11:23:56 -0500

    adding some projects

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnTrue()
+        {
+            var reservation = new Reservation();
+            var result = reservation.CanBeCancelledBy(new User { IsAdmin = true });
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+            var result = reservation.CanBeCancelledBy(user);
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnFalse()
+        {
+            var user = new User();
+            var reservation = new Reservation();
+            var anotherUser = new User();
+            var result = reservation.CanBeCancelledBy(anotherUser);
+            Assert.IsFalse(result);
+        }
+    }
+}
